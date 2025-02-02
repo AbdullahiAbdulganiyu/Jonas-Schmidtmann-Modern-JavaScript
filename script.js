66c004006d719336924bf5eebd741ada83071958
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-02-02 14:14:22 +0100

    using * to import all exported variables

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 // Importing Module
 
-import {
-  addToCart,
-  totalPrice as price,
-  totalquantity,
-} from './shoppingCart.js';
-console.log('Importing Module');
+// import {
+//   addToCart,
+//   totalPrice as price,
+//   totalquantity,
+// } from './shoppingCart.js';
+// console.log('Importing Module');
 
-addToCart('bread', 5);
+// addToCart('bread', 5);
 
-console.log(price, totalquantity);
+// console.log(price, totalquantity);
+
+import * as ShoppingCart from './shoppingCart.js';
+
+ShoppingCart.addToCart('bread', 5);
+console.log(ShoppingCart.totalPrice);
