29323d9e18b27f38e8a2ac197d113b937bd302f6
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-02-02 14:52:03 +0100

    Getting the result of an async function using top level await, courtesy strength of module

--- script.js
+++ script.js
@@ -37,5 +37,8 @@ const getLastPost = async function () {
   return { title: data.at(-1).title, text: data.at(-1).body };
 };
 
-const lastPost = getLastPost();
-lastPost.then(last => console.log(last));
+// const lastPost = getLastPost();
+// lastPost.then(last => console.log(last));
+
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
